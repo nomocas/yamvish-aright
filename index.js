4fa0ad9e9f90ad394956197d27d6e779015914b1
Gilles Coomans <gilles.coomans@gmail.com>
2015-11-05 14:43:34 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+/**  @author Gilles Coomans <gilles.coomans@gmail.com> */
+(function() {
+	'use strict';
+	var aright = require('aright'),
+		y = require('yamvish');
+
+	y.aright = aright;
+
+	y.Template.prototype.validate = function(path, rule) {
+		return this.exec(function(context) {
+			context.validate(path, rule);
+		}, true);
+	};
+
+	y.View.prototype.validate = y.Context.prototype.validate = function(path, rule) {
+		// subscribe on path then use validator to produce errors (if any) and place it in context.data.$error 
+		var self = this;
+
+		var applyValidation = function(type, path, value, key) {
+			var report;
+			if (type === 'push') // validate whole array ?
+				report = rule.validate(self.get(path));
+			else if (type !== 'removeAt')
+				report = rule.validate(value);
+			if (report !== true)
+				self.set('$error.' + path, report);
+			else
+				self.del('$error.' + path);
+		};
+
+		this.subscribe(path, applyValidation);
+		var val = this.get(path);
+		if (typeof val !== 'undefined')
+			applyValidation('set', path, val);
+		return this;
+	};
+
+	module.exports = aright;
+})();
